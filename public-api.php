c722ce641cfe60be0928ea37f3ea8d20856738cb
Sourov <sourovbuzz@gmail.com>
2022-01-21 03:15:18 +0600

    v

--- public-api.php
+++ public-api.php
@@ -184,7 +184,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
                     $myObj = new stdClass();
                     $myObj->status = true;
-                    $myObj->massage = "Password reset email was sent to your email ". $result['email'];
+                    $myObj->massage = "Password reset email was sent to your email ".hideEmailAddress($result['email']) ;
                     $myJSON = json_encode($myObj, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
                     echo $myJSON;
                 } else {
@@ -207,5 +207,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $myJSON = json_encode($myObj, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
             echo $myJSON;
         }
+    }
+}
+
+
+function hideEmailAddress($email)
+{
+    if(filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        list($first, $last) = explode('@', $email);
+        $first = str_replace(substr($first, '3'), str_repeat('*', strlen($first)-3), $first);
+        $last = explode('.', $last);
+        $last_domain = str_replace(substr($last['0'], '1'), str_repeat('*', strlen($last['0'])-1), $last['0']);
+        $hideEmailAddress = $first.'@'.$last_domain.'.'.$last['1'];
+        return $hideEmailAddress;
     }
 }
