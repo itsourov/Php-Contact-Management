d0dcb4f74d78633f554f45c07101ba239f83ca45
Sourov <sourovbuzz@gmail.com>
2022-01-20 23:35:08 +0600

    test change

--- add-friend.php
+++ add-friend.php
@@ -18,7 +18,7 @@ if ($_SESSION['login_user'] == null) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
        
 
-    <title>Add Friends</title>
+    <title>Add Friends no css</title>
 </head>
 
 <body>
